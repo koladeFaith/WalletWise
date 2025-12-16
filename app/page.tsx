f0d3686b967d8e110818019983ceeaf0e17c7e81
Kolade Faith <faithkolade0507@gmail.com>
2025-12-16 19:34:05 +0100

    Work done

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="">
  <h1 className="text-center bg-red-500">WalletWise</h1>
+ <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, fugit. Voluptatibus ut rem reprehenderit aliquam quasi? Ea earum eaque rerum temporibus dolores? Libero impedit accusantium consequuntur, deserunt voluptatem maiores facilis.</h3>
     </div>
   );
 }
